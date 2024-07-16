e247f0d77430492212229d232d1feae5616dd7a0
sunskycloud <q5503212@gmail.com>
2024-07-16 03:54:50 +0300

    normal outlet, normal logo, hero page fix fix

--- src/app/Providers/Provider.jsx
+++ src/app/Providers/Provider.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "@/pages/About/About.jsx";
 import Root from "@/pages/Root/Root.jsx";
+import Index from "@/pages/Index/Index.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
         children: [
+            {
+                index: true,
+                element: <Index />,
+            },
             {
                 path: "about",
                 element: <About />,
